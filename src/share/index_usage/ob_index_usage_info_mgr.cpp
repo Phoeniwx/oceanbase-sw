52b7e1eb80aaf64d9456e591b2dddb686093c521
steven_jiangxf <steven_jiangxf@hotmail.com>
2023-10-02 15:11:20 +0800

    use ccls style format code

--- src/share/index_usage/ob_index_usage_info_mgr.cpp
+++ src/share/index_usage/ob_index_usage_info_mgr.cpp
@@ -7,13 +7,13 @@
 #include "lib/ob_errno.h"
 #include "lib/oblog/ob_log_module.h"
 #include "lib/utility/ob_macro_utils.h"
+#include "observer/omt/ob_tenant_config_mgr.h" // ObTenantConfigGuard
 #include "share/ob_errno.h"
 #include "share/rc/ob_tenant_base.h"
-#include "observer/omt/ob_tenant_config_mgr.h" // ObTenantConfigGuard
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
-#include <algorithm>
 
 #define USING_LOG_PREFIX SERVER
 using namespace oceanbase::common;
@@ -23,13 +23,14 @@ namespace share {
 
 const char *OB_INDEX_USAGE_MANAGER = "hIndexUsageMgr";
 
-void ObIndexUsageOp::operator() (common::hash::HashMapPair<ObIndexUsageKey*, ObIndexUsageInfo*> &data) {
-  if (op_mode_==ObIndexUsageOpMode::UPDATE) {
+void ObIndexUsageOp::operator()(
+    common::hash::HashMapPair<ObIndexUsageKey *, ObIndexUsageInfo *> &data) {
+  if (op_mode_ == ObIndexUsageOpMode::UPDATE) {
     ret_code_ = OB_SUCCESS;
     data.second->ref_count++;
     data.second->last_used_time = time(NULL);
-    
-  } else if (op_mode_==ObIndexUsageOpMode::RESET) {
+
+  } else if (op_mode_ == ObIndexUsageOpMode::RESET) {
     ret_code_ = OB_SUCCESS;
     if (OB_ISNULL(old_info_)) {
       ret_code_ = OB_NULL_CHECK_ERROR;
@@ -38,25 +39,21 @@ void ObIndexUsageOp::operator() (common::hash::HashMapPair<ObIndexUsageKey*, ObI
       old_info_->ref_count = data.second->ref_count;
       old_info_->last_used_time = data.second->last_used_time;
       data.second->ref_count = 0;
-    }  
+    }
   }
-  is_called_ = true;  
+  is_called_ = true;
 }
 
 ObIndexUsageInfoMgr::ObIndexUsageInfoMgr()
-    : is_inited_(false),
-      is_destroying_(false),
-      index_usage_map_(),
-      init_lock_(),
-      destory_lock_(),
-      allocator_(MTL_ID()) 
-{}
+    : is_inited_(false), is_destroying_(false), index_usage_map_(),
+      init_lock_(), destory_lock_(), allocator_(MTL_ID()) {}
 
 ObIndexUsageInfoMgr::~ObIndexUsageInfoMgr() {}
 
 int ObIndexUsageInfoMgr::mtl_init(ObIndexUsageInfoMgr *&index_usage_mgr) {
   int ret = OB_SUCCESS;
-  index_usage_mgr = OB_NEW(ObIndexUsageInfoMgr, ObMemAttr(MTL_ID(), OB_INDEX_USAGE_MANAGER));
+  index_usage_mgr =
+      OB_NEW(ObIndexUsageInfoMgr, ObMemAttr(MTL_ID(), OB_INDEX_USAGE_MANAGER));
   if (OB_ISNULL(index_usage_mgr)) {
     ret = OB_ALLOCATE_MEMORY_FAILED;
     LOG_WARN("failed to alloc memory for ObIndexUsageInfoMgr", K(ret));
@@ -73,7 +70,6 @@ void ObIndexUsageInfoMgr::mtl_destroy(ObIndexUsageInfoMgr *&index_usage_mgr) {
     common::ob_delete(index_usage_mgr);
     index_usage_mgr = nullptr;
   }
-  
 }
 
 int ObIndexUsageInfoMgr::init() {
@@ -83,9 +79,11 @@ int ObIndexUsageInfoMgr::init() {
   if (OB_UNLIKELY(is_inited_)) {
     ret = OB_INIT_TWICE;
     LOG_WARN("init twice", K(ret));
-  } else if (OB_FAIL(index_usage_map_.create(DEFAULT_MAX_HASH_BUCKET_CNT, attr))) {
+  } else if (OB_FAIL(
+                 index_usage_map_.create(DEFAULT_MAX_HASH_BUCKET_CNT, attr))) {
     LOG_WARN("create hash map failed", K(ret));
-  } else if (OB_FAIL(allocator_.init(ObMallocAllocator::get_instance(), OB_MALLOC_NORMAL_BLOCK_SIZE, attr))) {
+  } else if (OB_FAIL(allocator_.init(ObMallocAllocator::get_instance(),
+                                     OB_MALLOC_NORMAL_BLOCK_SIZE, attr))) {
     LOG_WARN("init allocator failed", K(ret));
   } else {
     is_inited_ = true;
@@ -98,14 +96,17 @@ void ObIndexUsageInfoMgr::destory() {
     SpinWLockGuard guard(destory_lock_);
     is_destroying_ = true;
   }
-  for (ObIndexUsageHashMap::iterator it=index_usage_map_.begin(); it != index_usage_map_.end(); it++) {
+  for (ObIndexUsageHashMap::iterator it = index_usage_map_.begin();
+       it != index_usage_map_.end(); it++) {
     allocator_.free(it->first);
     allocator_.free(it->second);
   }
   index_usage_map_.destroy();
 }
 
-int ObIndexUsageInfoMgr::update(const int64_t database_id, const int64_t tenant_id, const int64_t index_table_id) {
+int ObIndexUsageInfoMgr::update(const int64_t database_id,
+                                const int64_t tenant_id,
+                                const int64_t index_table_id) {
   int ret = OB_SUCCESS;
   if (!GCONF._iut_enable) {
     return ret;
@@ -120,8 +121,8 @@ int ObIndexUsageInfoMgr::update(const int64_t database_id, const int64_t tenant_
   }
   ObIndexUsageKey temp_key(database_id, tenant_id, index_table_id);
   ObIndexUsageOp update_op(ObIndexUsageOpMode::UPDATE);
-  ObIndexUsageKey* new_key = nullptr;
-  ObIndexUsageInfo* new_info = nullptr;
+  ObIndexUsageKey *new_key = nullptr;
+  ObIndexUsageInfo *new_info = nullptr;
   SpinRLockGuard guard(destory_lock_);
   // suppose key exists, update it directory
   if (OB_FAIL(index_usage_map_.atomic_refactored(&temp_key, update_op))) {
@@ -129,11 +130,12 @@ int ObIndexUsageInfoMgr::update(const int64_t database_id, const int64_t tenant_
     if (ret == OB_HASH_NOT_EXIST) {
       if (OB_FAIL(alloc_new_record(temp_key, new_key, new_info))) {
         LOG_WARN("alloc memory for new index-usage record failed", K(ret));
-      } else if (OB_FAIL(index_usage_map_.set_or_update(new_key, new_info, update_op))) {
+      } else if (OB_FAIL(index_usage_map_.set_or_update(new_key, new_info,
+                                                        update_op))) {
         LOG_WARN("failed to set or update index-usage", K(ret));
-      } 
+      }
       // free new key if update is called
-      if (update_op.is_called() && OB_SUCCESS==update_op.get_ret()) {
+      if (update_op.is_called() && OB_SUCCESS == update_op.get_ret()) {
         allocator_.free(new_key);
         allocator_.free(new_info);
       }
@@ -144,30 +146,34 @@ int ObIndexUsageInfoMgr::update(const int64_t database_id, const int64_t tenant_
   return ret;
 }
 
-// sample ObIndexUsageInfo to pair_list, you need to free ObIndexUsageInfo* in pair_list later
-int ObIndexUsageInfoMgr::sample(common::ObList<ObIndexUsagePair,common::ObFIFOAllocator>& pair_list) {
+// sample ObIndexUsageInfo to pair_list, you need to free ObIndexUsageInfo* in
+// pair_list later
+int ObIndexUsageInfoMgr::sample(
+    common::ObList<ObIndexUsagePair, common::ObFIFOAllocator> &pair_list) {
   int ret = OB_SUCCESS;
-  const char* iut_mode = GCONF._iut_stat_collection_type;
+  const char *iut_mode = GCONF._iut_stat_collection_type;
   int64_t map_size = index_usage_map_.size();
   int64_t sample_count = index_usage_map_.size();
   if (strcmp(iut_mode, "SAMPLE") == 0) {
-    sample_count = sample_count * (SAMPLE_RATIO/100);
+    sample_count = sample_count * (SAMPLE_RATIO / 100);
   }
   std::srand(static_cast<uint32_t>(time(NULL)));
   ObIndexUsageOp reset_op(ObIndexUsageOpMode::RESET);
   int64_t index = 0;
-  for (ObIndexUsageHashMap::iterator it=index_usage_map_.begin(); it != index_usage_map_.end(); it++, index++) {
-    if (sample_count < map_size && rand()%(map_size-index)>=sample_count) {
+  for (ObIndexUsageHashMap::iterator it = index_usage_map_.begin();
+       it != index_usage_map_.end(); it++, index++) {
+    if (sample_count < map_size &&
+        rand() % (map_size - index) >= sample_count) {
       continue;
     }
     sample_count--;
-    void* buf = allocator_.alloc(sizeof(ObIndexUsageInfo));
+    void *buf = allocator_.alloc(sizeof(ObIndexUsageInfo));
     if (OB_ISNULL(buf)) {
       ret = OB_ALLOCATE_MEMORY_FAILED;
       LOG_WARN("failed to alloc memory for old info", K(ret));
       break;
     }
-    ObIndexUsageInfo* old_info = new(buf) ObIndexUsageInfo();
+    ObIndexUsageInfo *old_info = new (buf) ObIndexUsageInfo();
     reset_op.set_old_info(old_info);
     index_usage_map_.atomic_refactored(it->first, reset_op);
     ObIndexUsagePair pair;
@@ -177,7 +183,7 @@ int ObIndexUsageInfoMgr::sample(common::ObList<ObIndexUsagePair,common::ObFIFOAl
   return ret;
 }
 
-int ObIndexUsageInfoMgr::del(ObIndexUsageKey* key) {
+int ObIndexUsageInfoMgr::del(ObIndexUsageKey *key) {
   int ret = OB_SUCCESS;
   if (is_destroying()) {
     int ret = OB_ERROR;
@@ -185,38 +191,44 @@ int ObIndexUsageInfoMgr::del(ObIndexUsageKey* key) {
     return ret;
   }
   SpinRLockGuard guard(destory_lock_);
-  ObIndexUsageInfo *const * info_ptr_ptr = index_usage_map_.get(const_cast<ObIndexUsageKey *const>(key));
+  ObIndexUsageInfo *const *info_ptr_ptr =
+      index_usage_map_.get(const_cast<ObIndexUsageKey *const>(key));
   if (OB_ISNULL(info_ptr_ptr)) {
     LOG_WARN("index usage key not exists");
   } else {
-    if (OB_FAIL(index_usage_map_.erase_refactored(const_cast<ObIndexUsageKey *const>(key)))) {
+    if (OB_FAIL(index_usage_map_.erase_refactored(
+            const_cast<ObIndexUsageKey *const>(key)))) {
       LOG_WARN("failed to erase index usage key", K(ret));
     } else {
       if (OB_NOT_NULL(*info_ptr_ptr)) {
         allocator_.free(*info_ptr_ptr);
         allocator_.free(key);
-      }  
+      }
     }
   }
   return ret;
 }
 
-int ObIndexUsageInfoMgr::alloc_new_record(const ObIndexUsageKey& temp_key, ObIndexUsageKey* key, ObIndexUsageInfo* info){
+int ObIndexUsageInfoMgr::alloc_new_record(const ObIndexUsageKey &temp_key,
+                                          ObIndexUsageKey *key,
+                                          ObIndexUsageInfo *info) {
   int ret = OB_SUCCESS;
-  void* buf_key = allocator_.alloc(sizeof(ObIndexUsageKey));
-  void* buf_info = allocator_.alloc(sizeof(ObIndexUsageInfo));
+  void *buf_key = allocator_.alloc(sizeof(ObIndexUsageKey));
+  void *buf_info = allocator_.alloc(sizeof(ObIndexUsageInfo));
   if (OB_ISNULL(buf_key) || OB_ISNULL(buf_info)) {
     ret = OB_ALLOCATE_MEMORY_FAILED;
     LOG_WARN("failed to alloc index-usage memory", K(ret));
   } else {
-    key = new(buf_key) ObIndexUsageKey(temp_key.database_id, temp_key.tenant_id, temp_key.index_table_id);
-    info = new(buf_key) ObIndexUsageInfo(temp_key.index_table_id, 0, time(NULL));
+    key = new (buf_key) ObIndexUsageKey(
+        temp_key.database_id, temp_key.tenant_id, temp_key.index_table_id);
+    info =
+        new (buf_key) ObIndexUsageInfo(temp_key.index_table_id, 0, time(NULL));
   }
   if (OB_FAIL(ret)) {
-    if (OB_NOT_NULL(buf_key)){
+    if (OB_NOT_NULL(buf_key)) {
       allocator_.free(buf_key);
     }
-    if (OB_NOT_NULL(buf_info)){
+    if (OB_NOT_NULL(buf_info)) {
       allocator_.free(buf_info);
     }
   }
@@ -235,5 +247,5 @@ bool ObIndexUsageInfoMgr::is_destroying() {
   return is_destroying_;
 }
 
-}
-}
+} // namespace share
+} // namespace oceanbase
--- src/share/index_usage/ob_index_usage_info_mgr.h
+++ src/share/index_usage/ob_index_usage_info_mgr.h
@@ -2,46 +2,47 @@
 #ifndef SRC_SHARE_INDEX_USAGE_OB_INDEX_USAGE_INFO_MGR_H_
 #define SRC_SHARE_INDEX_USAGE_OB_INDEX_USAGE_INFO_MGR_H_
 
-#include <cstdint>
-#include <ctime>
-#include <mutex>
 #include "lib/allocator/ob_fifo_allocator.h"
 #include "lib/hash/ob_hashmap.h"
 #include "lib/hash/ob_hashutils.h"
+#include "lib/list/ob_list.h"
 #include "lib/lock/ob_spin_rwlock.h"
 #include "lib/lock/ob_tc_rwlock.h"
 #include "lib/ob_define.h"
 #include "lib/ob_errno.h"
 #include "lib/utility/ob_macro_utils.h"
 #include "share/ob_cascad_member.h"
-#include "lib/list/ob_list.h"
+#include <cstdint>
+#include <ctime>
+#include <mutex>
 
 namespace oceanbase {
 namespace share {
 
-enum ObIndexUsageOpMode
-{
+enum ObIndexUsageOpMode {
   UPDATE, // for update haspmap
-  RESET // for reset hashmap
+  RESET   // for reset hashmap
 };
 
 struct ObIndexUsageKey {
-  ObIndexUsageKey(int64_t database_id, int64_t tenant_id, int64_t index_table_id)
-      : database_id(database_id), tenant_id(tenant_id), index_table_id(index_table_id){}
-  ~ObIndexUsageKey(){}
+  ObIndexUsageKey(int64_t database_id, int64_t tenant_id,
+                  int64_t index_table_id)
+      : database_id(database_id), tenant_id(tenant_id),
+        index_table_id(index_table_id) {}
+  ~ObIndexUsageKey() {}
 
-  uint64_t hash() const
-  {
-    uint64_t hash_value = common::murmurhash(&database_id, 3*sizeof(int64_t), 0);
+  uint64_t hash() const {
+    uint64_t hash_value =
+        common::murmurhash(&database_id, 3 * sizeof(int64_t), 0);
     return hash_value;
   }
-  inline int hash(uint64_t &hash_val) const
-  {
+  inline int hash(uint64_t &hash_val) const {
     hash_val = hash();
     return OB_SUCCESS;
   }
   bool operator==(const ObIndexUsageKey &other) const {
-    return database_id == other.database_id && tenant_id == other.tenant_id && index_table_id == other.index_table_id;
+    return database_id == other.database_id && tenant_id == other.tenant_id &&
+           index_table_id == other.index_table_id;
   }
 
   int64_t database_id;
@@ -50,10 +51,12 @@ struct ObIndexUsageKey {
 };
 
 struct ObIndexUsageInfo {
-  ObIndexUsageInfo(int64_t data_table_id, int64_t ref_count, time_t last_used_time)
-      : data_table_id(data_table_id), ref_count(ref_count), last_used_time(last_used_time){}
-  ObIndexUsageInfo(){}
-  ~ObIndexUsageInfo(){}
+  ObIndexUsageInfo(int64_t data_table_id, int64_t ref_count,
+                   time_t last_used_time)
+      : data_table_id(data_table_id), ref_count(ref_count),
+        last_used_time(last_used_time) {}
+  ObIndexUsageInfo() {}
+  ~ObIndexUsageInfo() {}
   int64_t data_table_id;
   int64_t ref_count;
   time_t last_used_time;
@@ -62,45 +65,54 @@ struct ObIndexUsageInfo {
 // callback for update or reset map value
 class ObIndexUsageOp {
 public:
-  explicit ObIndexUsageOp(ObIndexUsageOpMode mode, ObIndexUsageInfo* old_info=nullptr) 
-    : op_mode_(mode), is_called_(false), ret_code_(common::OB_SUCCESS), old_info_(old_info) {}
-  virtual ~ObIndexUsageOp(){}
-  void operator() (common::hash::HashMapPair<ObIndexUsageKey*, ObIndexUsageInfo*> &data);
-  void set_old_info(ObIndexUsageInfo* old_info) {old_info_ = old_info;}
-  bool is_called() {return is_called_;}
-  int get_ret() {return ret_code_;}
+  explicit ObIndexUsageOp(ObIndexUsageOpMode mode,
+                          ObIndexUsageInfo *old_info = nullptr)
+      : op_mode_(mode), is_called_(false), ret_code_(common::OB_SUCCESS),
+        old_info_(old_info) {}
+  virtual ~ObIndexUsageOp() {}
+  void operator()(
+      common::hash::HashMapPair<ObIndexUsageKey *, ObIndexUsageInfo *> &data);
+  void set_old_info(ObIndexUsageInfo *old_info) { old_info_ = old_info; }
+  bool is_called() { return is_called_; }
+  int get_ret() { return ret_code_; }
+
 private:
   ObIndexUsageOpMode op_mode_;
   bool is_called_;
   int ret_code_;
-  ObIndexUsageInfo* old_info_;
+  ObIndexUsageInfo *old_info_;
   DISALLOW_COPY_AND_ASSIGN(ObIndexUsageOp);
 };
 
-class ObIndexUsageInfoMgr
-{
-typedef common::hash::ObHashMap<ObIndexUsageKey*, ObIndexUsageInfo*, common::hash::ReadWriteDefendMode> ObIndexUsageHashMap;
-static const int64_t SAMPLE_RATIO = 50;	// 采样模式下的采样比例，50 表示 50%
-static const int64_t DEFAULT_MAX_HASH_BUCKET_CNT = 10000;
+class ObIndexUsageInfoMgr {
+  typedef common::hash::ObHashMap<ObIndexUsageKey *, ObIndexUsageInfo *,
+                                  common::hash::ReadWriteDefendMode>
+      ObIndexUsageHashMap;
+  static const int64_t SAMPLE_RATIO = 50; // 采样模式下的采样比例，50 表示 50%
+  static const int64_t DEFAULT_MAX_HASH_BUCKET_CNT = 10000;
 
 public:
-  typedef common::hash::HashMapPair<const ObIndexUsageKey*, ObIndexUsageInfo*> ObIndexUsagePair;
+  typedef common::hash::HashMapPair<const ObIndexUsageKey *, ObIndexUsageInfo *>
+      ObIndexUsagePair;
   static int mtl_init(ObIndexUsageInfoMgr *&index_usage_mgr);
   static void mtl_destroy(ObIndexUsageInfoMgr *&index_usage_mgr);
   ObIndexUsageInfoMgr();
   ~ObIndexUsageInfoMgr();
 
 public:
-  int init(); 	// 申请map内存，在创建索引之后调用
-  void destory();	// 释放map，在observer析构时调用
-  int update(const int64_t database_id, const int64_t tenant_id, const int64_t index_table_id);
-  int del(ObIndexUsageKey* key);
-  int sample(common::ObList<ObIndexUsagePair,common::ObFIFOAllocator>& pair_list); // 采样哈希表
-  void release_node(ObIndexUsageInfo* info); // 用于task释放内存
+  int init();     // 申请map内存，在创建索引之后调用
+  void destory(); // 释放map，在observer析构时调用
+  int update(const int64_t database_id, const int64_t tenant_id,
+             const int64_t index_table_id);
+  int del(ObIndexUsageKey *key);
+  int sample(common::ObList<ObIndexUsagePair, common::ObFIFOAllocator>
+                 &pair_list);                // 采样哈希表
+  void release_node(ObIndexUsageInfo *info); // 用于task释放内存
 
 private:
   bool is_destroying();
-  int alloc_new_record(const ObIndexUsageKey& temp_key, ObIndexUsageKey* key, ObIndexUsageInfo* info);
+  int alloc_new_record(const ObIndexUsageKey &temp_key, ObIndexUsageKey *key,
+                       ObIndexUsageInfo *info);
 
 private:
   bool is_inited_;
@@ -111,6 +123,6 @@ private:
   common::ObFIFOAllocator allocator_;
 };
 
-}
-}
+} // namespace share
+} // namespace oceanbase
 #endif
